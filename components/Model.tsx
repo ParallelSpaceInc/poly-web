5e75443ca974a5904178bc0d3fdb7e7c6c097b5b
Taehyeong Kim <kth@pspace.ai>
2022-07-13 17:56:24 +0900

    url fix for server

--- components/Model.tsx
+++ components/Model.tsx
@@ -45,9 +45,9 @@ interface ModelViewerElement extends Element {
 
 const Model = () => {
   const info = {
-    src: "10.0.1.101:30201/models/국보275_도기_기마인물형_뿔잔/scene.gltf",
+    src: "http://127.0.0.1:30201/models/국보275_도기_기마인물형_뿔잔/scene.gltf",
     poster:
-      "10.0.1.101:30201/models/썸네일/PNG/형국보275 도기 기마인물형 뿔잔 1.png",
+      "http://127.0.0.1:30201/models/썸네일/PNG/형국보275 도기 기마인물형 뿔잔 1.png",
     alt: "국보275_도기_기마인물형_뿔잔",
     "shadow-intensity": "1",
     "camera-controls": "",
