59bca0835461c993d97b6aed0c37b0509ef6c800
Youngjae Lee <lyj@pspace.ai>
2022-07-13 10:36:29 +0900

    add header

---
+++ components/header.tsx
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Header(){
+
+    const [isOpenLoginCP,setIsOpenLoginCp] = useState(false);
+
+
+    return (
+        <div className={'relative'}>
+            <div className={'flex bg-indigo-800 w-full h-12 justify-between pl-10 pr-16 items-center text-amber-50'}>
+                <Link href={'/'}>
+                    <a className={'flex items-center'}>
+                        <Image src={'/210105_pspace_logo_W.png'} width={70} height={20}/>
+                    </a>
+                </Link>
+                <div className={'cursor-pointer'} onClick={()=>setIsOpenLoginCp(!isOpenLoginCP)}>LOGIN</div>
+            </div>
+        </div>
+
+    )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import "@styles/globals.css";
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
+import Header from "@components/header";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({Component, pageProps}: AppProps) {
+
+    return (
+        <>
+            <Header/>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp;
