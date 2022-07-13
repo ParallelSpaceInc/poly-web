dfc5808361a08ec9338e09474d83c0cb738eacf0
Youngjae Lee <lyj@pspace.ai>
2022-07-13 16:04:03 +0900

    add signIn with google, github

---
+++ pages/api/auth.ts
@@ -0,0 +1,6 @@
+import {supabase} from "@supabase/client";
+import {NextApiRequest, NextApiResponse} from "next";
+
+export default function handler(req : NextApiRequest, res : NextApiResponse) {
+    supabase.auth.api.setAuthCookie(req, res)
+}
