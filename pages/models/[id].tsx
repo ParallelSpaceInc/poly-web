1b3c9c440c98fc11f1cc3ccf1117054cc79cf769
Taehyeong Kim <kth@pspace.ai>
2022-07-13 16:17:04 +0900

    frame mobile model page

--- pages/models/[id].tsx
+++ pages/models/[id].tsx
@@ -1,7 +1,49 @@
 import type { NextPage } from "next";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
 const Model: NextPage = () => {
-  return <span>model viewer page</span>;
+  const router = useRouter();
+  const id = router.query.id;
+  return (
+    <div className="flex flex-col space-y-2">
+      <div className="flex bg-blue-900 h-10 p-2.5">
+        <Image
+          src="/logo.svg"
+          width={400}
+          height="10rm"
+          alt="pararell space logo"
+          className="justify-self-start block"
+        ></Image>
+      </div>
+      <div className="mx-8 space-y-2">
+        <input
+          className="p-1 pl-3 border-2 rounded-md border-blue-900 border-spacing-2 w-full"
+          placeholder="모델명 검색"
+        ></input>
+        <div className="h-64 bg-slate-400"></div>
+        <span className="block text-2xl mt-4">{id}</span>
+        <span className="text-slate-500 text-sm">카테고리 {">"} 국보</span>
+        <p className="text-slate-500 text-xs max-h-32 overflow-y-scroll">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non nibh
+          eleifend, fringilla massa eget, placerat nisl. Morbi lectus erat,
+          tristique in euismod eu, faucibus sed neque. Maecenas aliquam pharetra
+          dictum. Suspendisse eu dignissim est, id euismod libero. In
+          sollicitudin, orci id lobortis luctus, libero leo hendrerit quam, eu
+          vestibulum nunc sem a risus. Morbi lacus libero, rhoncus quis arcu
+          eget, pharetra feugiat metus. Pellentesque habitant morbi tristique
+          senectus et netus et malesuada fames ac turpis egestas. Pellentesque
+          consequat sed mi nec accumsan. Nunc gravida condimentum risus nec
+          facilisis. Cras risus tortor, consequat non consectetur non, aliquam
+          non augue. Class aptent taciti sociosqu ad litora torquent per conubia
+          nostra, per inceptos himenaeos. Mauris consequat metus vitae est
+          tempor placerat. Praesent justo orci, sagittis ut lacus id, hendrerit
+          blandit tortor. Quisque non condimentum nisi. Duis pretium erat odio,
+          in lobortis arcu luctus id. In blandit nisl nec erat aliquet pulvinar.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Model;
