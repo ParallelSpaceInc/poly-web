b6127f455e5dd96dfbba2ca6fa1841b3ac90aa95
Taehyeong Kim <kth@pspace.ai>
2022-07-14 09:58:57 +0900

    redirect to models page from root page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  useEffect(() => {
+    router.replace("/models");
+  }, [router]);
+
   return <span>root page</span>;
 };
 
