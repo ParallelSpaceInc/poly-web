99e620b1ab508ec66b876f0a0278d4cc7da4cb81
Taehyeong Kim <kth@pspace.ai>
2022-07-14 11:32:19 +0900

    lmpl : make api for fetching models info

--- pages/api/models/index.ts
+++ pages/api/models/index.ts
@@ -1,6 +1,26 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+type ModelInfo = {
+  id: number;
+  name: string;
+  modelSrc: string;
+  thumbnailSrc: string;
+};
+
+const makeModelInfo: (id: number, name: string) => ModelInfo = (id, name) => ({
+  id,
+  name,
+  modelSrc: `/getResource/models/${id}/scene.gltf`,
+  thumbnailSrc: `/getResource/models/${id}/thumbnail.png`,
+});
+
+const modelList: ModelInfo[] = Array.from(Array(10).keys()).map((key) =>
+  makeModelInfo(key, `Model Sample ${key}`)
+);
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
-) {}
+) {
+  res.status(200).json(modelList);
+}
