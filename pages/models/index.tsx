907d5810061031e379b1e4fe97ee305693c18681
Taehyeong Kim <kth@pspace.ai>
2022-07-14 10:17:01 +0900

    style:hange theme color to gray from blue

--- pages/models/index.tsx
+++ pages/models/index.tsx
@@ -3,17 +3,17 @@ import type { NextPage } from "next";
 const ModelsMainPage: NextPage = () => {
   return (
     <div className="flex flex-col space-y-2 ">
-      <div className="flex bg-blue-900 h-10 p-2.5"></div>
+      <div className="flex bg-slate-500 h-10 p-2.5"></div>
       <div className="mx-8">
         <input
-          className="p-1 pl-3 border-2 rounded-md border-blue-900 border-spacing-2 w-full"
+          className="p-1 pl-3 border-2 rounded-md border-slate-500 border-spacing-2 w-full"
           placeholder="Find model"
         ></input>
         <div className="flex flex-col mx-10 text-sm"></div>
         <div className="grid grid-cols-2 p-3 gap-3 max-h-full overflow-y-hidden">
           {Array.from(Array(10).keys()).map((el, i) => (
             <div key={i}>
-              <div className="bg-slate-400 h-32 rounded-lg"></div>
+              <div className="border-2 border-slate-400 h-32 rounded-lg"></div>
             </div>
           ))}
         </div>
