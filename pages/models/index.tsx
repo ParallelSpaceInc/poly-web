2bea7c2d773b17cb668be38dadb900a5ef1b7938
Taehyeong Kim <kth@pspace.ai>
2022-07-15 13:48:42 +0900

    page : add model name on index page

--- pages/models/index.tsx
+++ pages/models/index.tsx
@@ -22,17 +22,24 @@ const ModelsMainPage: NextPage = () => {
         <div className="mt-10 grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
           {!loading && modelInfos ? (
             modelInfos.map((info, i) => (
-              <div className="block  aspect-[4/3] relative" key={i}>
-                <Link href={`/models/${info.id}`}>
-                  <Image
-                    src={info.thumbnailSrc}
-                    alt={info.name}
-                    layout="fill"
-                    objectFit="cover"
-                    className="rounded-lg"
-                  />
-                </Link>
-              </div>
+              <Link key={i} href={`/models/${info.id}`}>
+                <div className="flex flex-col relative cursor-pointer">
+                  <div className="block  aspect-[4/3] relative">
+                    <Image
+                      src={info.thumbnailSrc}
+                      alt={info.name}
+                      layout="fill"
+                      objectFit="cover"
+                      className="rounded-lg"
+                    />
+                  </div>
+                  <div className="flex items-end justify-between">
+                    <p className="block mt-2 text-gray-900 truncate">
+                      {info.name}
+                    </p>
+                  </div>
+                </div>
+              </Link>
             ))
           ) : (
             <span>Loading...</span>
