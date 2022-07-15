99ee9fc9c565d116dc0af0a052fa4cf20c2356ff
Youngjae Lee <lyj@pspace.ai>
2022-07-15 11:38:30 +0900

    fix: indent

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,83 +6,84 @@ import {useEffect, useState} from "react";
 import {useRouter} from "next/router";
 
 export interface UserData {
-    user_id: string | undefined,
-    name: string,
-    email: string | undefined,
-    avatar: string,
-    created_at: string | undefined
+  user_id: string | undefined,
+  name: string,
+  email: string | undefined,
+  avatar: string,
+  created_at: string | undefined
 }
 
 function MyApp({Component, pageProps}: AppProps) {
-    const router = useRouter();
+  const router = useRouter();
 
-    const [user, setUser] = useState<UserData | undefined>(undefined)
+  const [user, setUser] = useState<UserData | undefined>(undefined)
 
-    const session = supabase.auth.session();
-    useEffect(() => {
-        if (session) {
-            const userData: UserData = {
-                user_id: session?.user?.id,
-                name: session?.user?.user_metadata.user_name || session?.user?.user_metadata.full_name,
-                email: session?.user?.email,
-                avatar: session?.user?.user_metadata.avatar_url,
-                created_at: session?.user?.created_at
-            }
-            setUser(userData)
+  const session = supabase.auth.session();
+
+  useEffect(() => {
+    if (session) {
+      const userData: UserData = {
+        user_id: session?.user?.id,
+        name: session?.user?.user_metadata.user_name || session?.user?.user_metadata.full_name,
+        email: session?.user?.email,
+        avatar: session?.user?.user_metadata.avatar_url,
+        created_at: session?.user?.created_at
+      }
+      setUser(userData)
+    }
+  }, [session])
+
+  useEffect(() => {
+    const {data: authListener} = supabase.auth.onAuthStateChange(async (event, session) => {
+
+      if (event === 'SIGNED_IN') {
+
+        const loggedInUser = supabase.auth.user()
+
+        fetch('/api/auth', {
+          method: 'POST',
+          headers: new Headers({'Content-Type': 'application/json'}),
+          credentials: 'same-origin',
+          body: JSON.stringify({event, session}),
+        }).then((res) => res.json())
+
+        const {data} = await supabase.from('profiles').select('*').eq('user_id', loggedInUser?.id);
+
+        const userInfo: UserData = {
+          user_id: loggedInUser?.id,
+          name: loggedInUser?.user_metadata.user_name || loggedInUser?.user_metadata.full_name,
+          email: loggedInUser?.email,
+          avatar: loggedInUser?.user_metadata.avatar_url,
+          created_at: loggedInUser?.created_at
         }
-    }, [session])
 
-    useEffect(() => {
-        const {data: authListener} = supabase.auth.onAuthStateChange(async (event, session) => {
+        if (!data?.length && loggedInUser) {
 
-            if (event === 'SIGNED_IN') {
-
-                const loggedInUser = supabase.auth.user()
-
-                fetch('/api/auth', {
-                    method: 'POST',
-                    headers: new Headers({'Content-Type': 'application/json'}),
-                    credentials: 'same-origin',
-                    body: JSON.stringify({event, session}),
-                }).then((res) => res.json())
-
-                const {data} = await supabase.from('profiles').select('*').eq('user_id', loggedInUser?.id);
-
-                const userInfo: UserData = {
-                    user_id: loggedInUser?.id,
-                    name: loggedInUser?.user_metadata.user_name || loggedInUser?.user_metadata.full_name,
-                    email: loggedInUser?.email,
-                    avatar: loggedInUser?.user_metadata.avatar_url,
-                    created_at: loggedInUser?.created_at
-                }
-
-                if (!data?.length && loggedInUser) {
-
-                    await supabase.from('profiles').insert([
-                        userInfo
-                    ])
-                }
-
-                setUser(userInfo)
-
-            } else if (event === 'SIGNED_OUT') {
-                setUser(undefined)
-
-                await router.push('/')
-            }
-        })
-
-        return () => {
-            authListener?.unsubscribe();
+          await supabase.from('profiles').insert([
+            userInfo
+          ])
         }
-    }, [])
 
-    return (
-        <>
-            <Header user={user}/>
-            <Component {...pageProps} />
-        </>
-    )
+        setUser(userInfo)
+
+      } else if (event === 'SIGNED_OUT') {
+        setUser(undefined)
+
+        await router.push('/')
+      }
+    })
+
+    return () => {
+      authListener?.unsubscribe();
+    }
+  }, [])
+
+  return (
+    <>
+      <Header user={user}/>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp;
