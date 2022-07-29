134eccf2e3b1e657bd95c0b3385f6b4fd6153ef7
Taehyeong Kim <kth@pspace.ai>
2022-07-29 18:03:54 +0900

    refactoring : make prisma client singliy exist

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,8 @@
+import prisma from "@libs/server/prisma";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-
-const prisma = new PrismaClient();
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
