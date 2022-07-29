419c3f569bd5516e74e2a31b9ca70c548e77f8bb
Taehyeong Kim <kth@pspace.ai>
2022-07-29 13:32:14 +0900

    refactoring : use nextauth for client system
    aa

--- components/header.tsx
+++ components/header.tsx
@@ -1,32 +1,24 @@
 import Login from "@components/login";
-import { supabase } from "@supabase/client";
+import { signOut, useSession } from "next-auth/react";
 import { useState } from "react";
-import { UserData } from "../pages/_app";
 
-interface Props {
-  user: UserData | undefined;
-}
-
-export default function Header({ user }: Props) {
+export default function Header() {
+  const { data: session, status } = useSession();
   const [isOpenLoginCP, setIsOpenLoginCp] = useState(false);
 
   const closeLoginBox = (): void => {
     setIsOpenLoginCp(false);
   };
 
-  const logOut = async () => {
-    await supabase.auth.signOut();
-  };
-
   return (
     <div className="relative">
       <div className="flex justify-end bg-gray-400 w-full h-12  pl-10 pr-8 md:pr-16 items-center text-amber-50">
-        {user !== undefined ? (
+        {status === "authenticated" ? (
           <div className="flex justify-between items-center space-x-5">
             <div>
-              <p>{user.name}님</p>
+              <p>{session?.user?.name}님</p>
             </div>
-            <button className="border-2 px-2 rounded" onClick={logOut}>
+            <button className="border-2 px-2 rounded" onClick={() => signOut()}>
               log out
             </button>
           </div>
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID ?? "",
+      clientSecret: process.env.GOOGLE_SECRET ?? "",
+    }),
+  ],
+});
