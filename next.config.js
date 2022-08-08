fb826e02a3a7f3145d9a3837cc8888342ac08a26
Taehyeong Kim <kth@pspace.ai>
2022-08-08 13:16:49 +0900

    package : install extract-zip and setting for using fs.

--- next.config.js
+++ next.config.js
@@ -15,6 +15,12 @@ const nextConfig = {
       },
     ];
   },
-};
 
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
+};
 module.exports = nextConfig;
