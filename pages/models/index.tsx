6fe4a4c34ad4132eb4b3aaee8c1769f880c243bf
Taehyeong Kim <kth@pspace.ai>
2022-08-10 15:54:45 +0900

    impl : connect mainpage with db

--- pages/models/index.tsx
+++ pages/models/index.tsx
@@ -1,5 +1,7 @@
 import { ModelInfo } from "@customTypes/model";
 import type { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
 import useSWR from "swr";
 
 const ModelsMainPage: NextPage = () => {
@@ -21,9 +23,9 @@ const ModelsMainPage: NextPage = () => {
           </div>
         </div>
         <div className="mt-10 grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-          {/* {!loading && modelInfos ? (
+          {!loading && modelInfos ? (
             modelInfos.map((info, i) => (
-              <Link key={i} href={`/models/${info.id}`}>s
+              <Link key={i} href={`/models/${info.id}`}>
                 <div className="flex flex-col relative cursor-pointer">
                   <div className="block  aspect-[4/3] relative rounded-lg shadow-md">
                     <Image
@@ -44,7 +46,7 @@ const ModelsMainPage: NextPage = () => {
             ))
           ) : (
             <span>Loading...</span>
-          )} */}
+          )}
         </div>
       </div>
     </div>
