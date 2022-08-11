81a7a180fc46b047eaf62e8932d4a742efccbd54
Taehyeong Kim <kth@pspace.ai>
2022-08-11 11:39:07 +0900

    impl : library function to get user record on server side.

--- libs/server/prismaClient.ts
+++ libs/server/prismaClient.ts
@@ -1,9 +1,22 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest } from "next";
+import { getSession } from "next-auth/react";
 
 declare global {
-  var client: PrismaClient;
+  var prismaClient: PrismaClient;
 }
 
-global.client ??= new PrismaClient();
+global.prismaClient ??= new PrismaClient();
 
-export default global.client;
+export default global.prismaClient;
+
+export const getUser = async (req: NextApiRequest) => {
+  const session = await getSession({ req });
+  const email = session?.user?.email;
+  const user = email
+    ? await global.prismaClient.user.findUnique({
+        where: { email },
+      })
+    : null;
+  return user;
+};
