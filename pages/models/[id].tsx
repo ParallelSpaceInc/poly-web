778d172b89c946b76a61c6f7faed0fd069cac02e
Taehyeong Kim <kth@pspace.ai>
2022-08-19 09:51:24 +0900

    css : refine page style

--- pages/models/[id].tsx
+++ pages/models/[id].tsx
@@ -21,52 +21,55 @@ const ModelPage: NextPage = () => {
 
   return (
     <div className="flex flex-col">
-      <div className="flex-1 mt-10 relative w-full px-6 mx-auto max-w-7xl lg:px-8">
+      <div className="flex-1 mt-10 relative w-full px-10 mx-auto max-w-7xl lg:px-20">
         <input
           className="p-1 pl-3 lg:text-3xl border-2 rounded-md border-slate-500 border-spacing-2 w-full"
           placeholder="Find model"
         ></input>
-        <div className="aspect-[4/3] w-full max-w-5xl mx-auto mt-8">
-          {!model.loading ? <Model info={model.data} /> : "Loading..."}
-        </div>
         <span className="block text-2xl mt-4 md:text-3xl lg:text-4xl">
           {!model.loading ? model.data.name : ""}
         </span>
+        <div className="block my-10 sm:grid sm:grid-cols-3 gap-x-4 gap-y-8">
+          <div className="aspect-[4/3] w-full col-span-2 max-w-5xl mx-auto mt-8">
+            {/* <div className="relative min-w-full min-h-full col-span-2"> */}
+            {!model.loading ? <Model info={model.data} /> : "Loading..."}
+          </div>
+          <div className="flex flex-col space-y-3 mt-10 ">
+            {hasRight(
+              { method: "read", theme: "model" },
+              user.data,
+              model.data
+            ) ? (
+              <button
+                onClick={() => {
+                  router.push(`/getResource/models/${modelId}/model.zip`);
+                }}
+                className=" text-white bg-slate-700 h-10"
+              >
+                download
+              </button>
+            ) : null}
+            {hasRight(
+              { method: "delete", theme: "model" },
+              user.data,
+              model.data
+            ) ? (
+              <button
+                onClick={() => callDeleteAPI(modelId, router)}
+                className=" text-white bg-red-500 h-10"
+              >
+                delete
+              </button>
+            ) : null}
+          </div>
+        </div>
+
         <span className="block text-lg mt-6 md:text-xl lg:text-xl text-slate-600">
           {!model.loading ? `Category > ${model.data.category}` : ""}
         </span>
-        <span className="block mt-10 text-slate-500 text-sm md:text-lg lg:text-xl">
+        <span className="block mt-10 text-slate-500 text-md md:text-lg lg:text-xl">
           {!model.loading ? model.data.description : ""}
         </span>
-        <p className="my-2 max-w-3xl mr-auto bg-slate-100 p-2 text-slate-500 text-xs md:text-base max-h-64 overflow-y-auto"></p>
-        <div className="flex space-x-3 mt-10 ">
-          {hasRight(
-            { method: "delete", theme: "model" },
-            user.data,
-            model.data
-          ) ? (
-            <button
-              onClick={() => callDeleteAPI(modelId, router)}
-              className="m-auto bg-slate-400 h-10"
-            >
-              delete
-            </button>
-          ) : null}
-          {hasRight(
-            { method: "read", theme: "model" },
-            user.data,
-            model.data
-          ) ? (
-            <button
-              onClick={() => {
-                router.push(`/getResource/models/${modelId}/model.zip`);
-              }}
-              className="m-auto bg-slate-400 h-10"
-            >
-              download
-            </button>
-          ) : null}
-        </div>
       </div>
     </div>
   );
