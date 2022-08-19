5718dafa0a84c3ba26eabe39b947afc010331038
Taehyeong Kim <kth@pspace.ai>
2022-08-19 15:36:27 +0900

    css : use Wrapper

--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Wrapper({ children }: React.PropsWithChildren) {
   return (
     <div className="flex flex-col">
-      <div className="flex-1 mt-10 relative w-full px-6 mx-auto max-w-7xl lg:px-8">
+      <div className="flex-1 mt-10 relative w-full px-10 mx-auto max-w-7xl lg:px-20">
         {children}
       </div>
     </div>
