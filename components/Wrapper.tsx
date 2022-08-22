d35f84ba257b14960db37550755201c4f9a1781c
Taehyeong Kim <kth@pspace.ai>
2022-08-22 15:05:45 +0900

    css : refine ui
    1. close modal on clicking outside of modal
    2. prevent wheel up, down when modal is on.
    3. little css adjust.
    4. adjust wrapper css

--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Wrapper({ children }: React.PropsWithChildren) {
   return (
     <div className="flex flex-col">
-      <div className="flex-1 my-10 w-full px-10 mx-auto max-w-7xl lg:px-20">
+      <div className="flex-1 mt-10 relative w-full px-6 mx-auto max-w-7xl lg:px-8">
         {children}
       </div>
     </div>
