cf569adbce6854261758b8d654d738fab331f0b6
Youngjae Lee <lyj@pspace.ai>
2022-09-06 16:46:20 +0900

    page : modelInfos to control in state

--- pages/models/index.tsx
+++ pages/models/index.tsx
@@ -1,15 +1,26 @@
 import SearchBar from "@components/Search";
 import Thumbnails from "@components/Thumbnails";
 import Wrapper from "@components/Wrapper";
+import { ModelInfo } from "@customTypes/model";
 import { useModelInfos } from "@libs/client/AccessDB";
 import type { NextPage } from "next";
+import { useState } from "react";
+
+export interface ModelInfos {
+  loading: boolean,
+  data: ModelInfo[] | undefined,
+  error: any
+}
 
 const ModelsMainPage: NextPage = () => {
-  const models = useModelInfos();
+  const [models, setModels] = useState<ModelInfos>()
+  const modelsInfos = useModelInfos();
+
   return (
+
     <Wrapper>
-      <SearchBar />
-      <Thumbnails loading={models.loading} modelInfos={models.data} />
+      <SearchBar setModels={setModels} />
+      <Thumbnails loading={models ? models?.loading ?? false : modelsInfos.loading} modelInfos={models ? models?.data ?? [] : modelsInfos.data} />
     </Wrapper>
   );
 };
