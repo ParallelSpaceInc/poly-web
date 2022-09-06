21fe37b1a4f6614211124bbc55721e56f7719986
Taehyeong Kim <kth@pspace.ai>
2022-09-06 16:35:13 +0900

    refactor : extract functions from previous upload function

--- libs/server/ServerFileHandling.ts
+++ libs/server/ServerFileHandling.ts
@@ -1,5 +1,7 @@
 import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { ValidatorInfo } from "@customTypes/model";
 import s3client from "@libs/server/s3client";
+import { Model } from "@prisma/client";
 
 import extract from "extract-zip";
 import formidable from "formidable";
@@ -10,7 +12,7 @@ import {
   rename,
   statSync,
 } from "fs";
-import { readdir, stat } from "fs/promises";
+import { readdir, readFile, stat } from "fs/promises";
 import { validateBytes } from "gltf-validator";
 import path, { join } from "path";
 
@@ -19,64 +21,135 @@ type FormidableResult = {
   fields: formidable.Fields;
   files: formidable.Files;
 };
-export const extractZipThenSendToS3 = async (
+
+//
+
+// devide multiple request and not multiple request -> [form, formidable.Files(Array)]
+
+// for each upload requirement -----------------------
+
+// param : form(about model)?, formidable.File
+
+// 0. generate uuid
+
+// 0. update Model from form. do nothing if undefined. (name, tags, description, category)
+
+// 1. extractZip (name)
+export async function extractZip(
   uuid: string,
-  formidable: FormidableResult
-): Promise<{
-  model: string;
-  thumbnail: string;
-  modelInfo: object;
-  totalSize: number;
-  modelUsdz: string;
-}> => {
-  const fileInfo = await getFileInfo(formidable.files);
-  const filePath = `/tmp/${uuid}`;
-  let res = {
-    model: "",
-    thumbnail: "",
-    modelInfo: {},
-    totalSize: 0,
-    modelUsdz: "",
-  };
-  await extract(fileInfo.path, { dir: filePath });
-  res.totalSize = await dirSize(filePath);
-  rename(fileInfo.path, join(filePath, "model.zip"), (err) => {
+  formidableFiles: formidable.Files
+) {
+  const fileInfo = await getFileInfo(formidableFiles);
+  if (path.extname(fileInfo.loadedFile) !== ".zip") {
+    throw Error("File is not .zip");
+  }
+  const newPath = `/tmp/${uuid}`;
+  const filenameWithoutExt = trimExt(fileInfo.originalName);
+  const newZipPath = join(newPath, "model.zip");
+  await extract(fileInfo.loadedFile, { dir: newPath });
+  rename(fileInfo.loadedFile, newZipPath, (err) => {
     if (err) throw err;
   });
+  const zipSize = statSync(newZipPath).size;
+  return { newPath, filenameWithoutExt, zipSize };
+}
+// name, zipSize
+
+type NotRequired<T> = {
+  [P in keyof T]+?: T[P];
+};
+
+type OptionalModel = NotRequired<Model>;
+
+function trimExt(name: string) {
+  if (!name.includes(".")) {
+    return name;
+  }
+  return name.split(".").slice(0, -1).join(".");
+}
+
+// 2-1 Update Model from dir (vertex, triangle, gltf length)
+export async function getGltfInfo(
+  gltf: string
+): Promise<ValidatorInfo.Resource> {
+  const asset = readFileSync(gltf);
+  return validateBytes(new Uint8Array(asset))
+    .then((report: ValidatorInfo.RootObject) => {
+      return report;
+    })
+    .catch((e: any) => {
+      throw e;
+    });
+}
+// vertex=, tri, model size
+
+// 2-2 update Model from dir (name, thumbnail. usdz, usdzSize, zipSize, description)
+export async function getModelFromDir(dirPath: string): Promise<OptionalModel> {
+  let model: OptionalModel = {};
+  const files = await getFilesPath(dirPath);
+  files.forEach(async (file) => {
+    const relativeFileName = path.relative(dirPath, file);
+    if (["scene.gltf", "scene.glb"].includes(relativeFileName)) {
+      model.modelFile = relativeFileName;
+    }
+    if ("thumbnail.png" === relativeFileName) {
+      model.thumbnail = relativeFileName;
+    }
+    if ("scene.usdz" === relativeFileName) {
+      model.modelUsdz = relativeFileName;
+      model.usdzSize = BigInt(statSync(file).size);
+    }
+    if ("description.txt" === relativeFileName) {
+      model.description = await readTextFile(file);
+    }
+  });
+  return model;
+}
+//name??=, thum??= ...
+
+//update if not exist
+export function updateModel(target: OptionalModel, newObject: OptionalModel) {
+  return Object.assign(newObject, target);
+}
+//
+
+// 3 chech Model requirement
+export function checkModel(model: OptionalModel) {
+  if (model.category) {
+    throw Error("category is not exist");
+  }
+  if (model.id) {
+    throw Error("model is not exist");
+  }
+  if (model.name) {
+    throw Error("name is not exist");
+  }
+}
+
+// 4-a update db
+// use prisma code
+
+// 4-b sendToS3
+export async function uploadModelToS3(dirPath: string, res: Model) {
   await Promise.all(
     (
-      await getFilesPath(filePath)
+      await getFilesPath(dirPath)
     ).map((file) => {
-      const type = path.extname(file);
-      const filename = path.extname(file);
-      if ([".gltf", ".glb"].includes(type)) {
-        res.model = path.basename(file);
-        const asset = readFileSync(file);
-        validateBytes(new Uint8Array(asset))
-          .then((report: object) => {
-            res.modelInfo = report;
-          })
-          .catch((e: any) => {
-            console.log(e);
-          });
-      }
-      if ("thumbnail.png" === filename) {
-        res.thumbnail = "thumbnail.png";
-      }
-      if ([".usdz"].includes(type)) {
-        res.modelUsdz = path.basename(file);
-      }
       const stream = createReadStream(file);
       const filesParams = {
         Bucket: process.env.S3_BUCKET,
-        Key: join(`models/${uuid}`, path.relative(filePath, file)),
+        Key: join(`models/${res.id}`, path.relative(dirPath, file)),
         Body: stream,
       };
       return s3client.send(new PutObjectCommand(filesParams));
     })
   );
-  return res;
-};
+}
+
+function readTextFile(textFile: string) {
+  const buffer = readFile(textFile, { encoding: "utf-8" });
+  return buffer;
+}
 
 const getFileInfo = (fileData: formidable.Files) => {
   if (typeof fileData === "string") {
@@ -87,11 +160,31 @@ const getFileInfo = (fileData: formidable.Files) => {
   }
   const parsed = fileData.file.toJSON();
   return Promise.resolve({
-    name: parsed.originalFilename ?? "noName",
-    path: parsed.filepath,
+    originalName: parsed.originalFilename ?? "noName",
+    loadedFile: parsed.filepath,
   });
 };
 
+const getFilesPath: (dir: string) => Promise<string[]> = async (
+  dir: string
+) => {
+  const files = readdirSync(dir);
+  const res = await Promise.all(
+    files.map(async (file) => {
+      const filePath = path.join(dir, file);
+      const stats = statSync(filePath);
+      if (stats.isDirectory()) return getFilesPath(filePath);
+      else return filePath;
+    })
+  );
+  if (res === undefined) return [];
+  return res.reduce<string[]>(
+    (all, folderContents) => all.concat(folderContents),
+    []
+  );
+};
+
+// useless for now.
 const dirSize: (dir: string) => Promise<number> = async (dir: string) => {
   const files = await readdir(dir, { withFileTypes: true });
 
@@ -112,23 +205,4 @@ const dirSize: (dir: string) => Promise<number> = async (dir: string) => {
   return (await Promise.all(paths))
     .flat(Infinity)
     .reduce((i, size) => i + size, 0);
-};
-
-const getFilesPath: (dir: string) => Promise<string[]> = async (
-  dir: string
-) => {
-  const files = readdirSync(dir);
-  const res = await Promise.all(
-    files.map(async (file) => {
-      const filePath = path.join(dir, file);
-      const stats = statSync(filePath);
-      if (stats.isDirectory()) return getFilesPath(filePath);
-      else return filePath;
-    })
-  );
-  if (res === undefined) return [];
-  return res.reduce<string[]>(
-    (all, folderContents) => all.concat(folderContents),
-    []
-  );
 };
