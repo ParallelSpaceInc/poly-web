d20e5d3ba8ef4140d2f235b962decfb677750ae0
Youngjae Lee <lyj@pspace.ai>
2022-09-06 16:48:09 +0900

    feafure: sorting models by sortType and isDesc

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,11 +1,100 @@
-function SearchBar() {
+import Image from "next/image";
+import { ModelInfos } from "pages/models";
+import { Dispatch, SetStateAction, useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface props {
+  setModels: Dispatch<SetStateAction<ModelInfos | undefined>>
+}
+type sortType = "Last Added" | "Size" | "Alphabetic"
+type srotTypes = sortType[]
+function SearchBar(props: props) {
+
+  const sortTypes: srotTypes = ["Last Added", "Size", "Alphabetic"]
+
+  const { register } = useForm();
+
+
+  const [currentSortType, setCurrentSortType] = useState<string>(sortTypes[0])
+  const [isClickSort, setIsClickSort] = useState<boolean>(false);
+  const isDescOfLastAdded = useState<boolean>(true)
+  const isDescOfSize = useState<boolean>(true)
+  const isDescOfAlphabetic = useState<boolean>(false)
+
+
+
+  const sortingModel = async (type: string) => {
+
+    const [isDescState, setIsDesc] = sortTypeState(type)
+    let isDesc = isDescState
+    if (type === currentSortType) {
+      setIsDesc(!isDesc)
+      isDesc = !isDesc
+    }
+    setCurrentSortType(type)
+
+    const { data, error } = await fetch(`/api/models?sort=${type.concat(`,${isDesc}`)}`, {
+      method: "GET",
+    }).then((res) => res.json())
+    const loading = !data && !error;
+    props.setModels({
+      loading,
+      data,
+      error
+    })
+
+    setIsClickSort(false)
+  }
+
+  const sortTypeState = (type: string): [boolean, Dispatch<SetStateAction<boolean>>] => {
+    switch (type) {
+      case "Last Added":
+        return isDescOfLastAdded
+      case "Size":
+
+        return isDescOfSize
+      case "Alphabetic":
+
+        return isDescOfAlphabetic
+      default:
+        return isDescOfLastAdded
+    }
+
+  }
+
   return (
-    <div className="block mt-6 relative">
-      <input
-        className="p-2 pl-4 w-full ring-2 ring-gray-300 focus:ring-indigo-500 text-xl rounded-md outline-none"
-        placeholder="Find model"
-      ></input>
-    </div>
+
+    <div className="w-full flex justify-between mt-6">
+      <div className="w-3/4 relative ring-2 ring-gray-300 focus:ring-indigo-500 rounded-md outline-none flex justify-center items-center">
+        <form className="p-2 border-none rounded-md outline-none pl-4 flex-1 text-xl">
+          <input
+            className="w-full"
+            placeholder="Find model"
+            {...register("findModelName")}
+          />
+        </form>
+
+        <div className="pr-3 flex items-center cursor-pointer">
+          <Image src="/searchIcon.png" width="20px" height="20px" alt="find model" />
+        </div>
+      </div>
+      <div className="rounded-md ring-2 ring-gray-300 w-1/6 text-center flex justify-center items-center relative cursor-pointer">
+        <div className="w-full font-semibold text-gray-500 h-full text-sm border-none outline-none flex justify-center items-center"
+          onClick={() => {
+            setIsClickSort(!isClickSort)
+          }}
+        >
+          <p>{currentSortType} {sortTypeState(currentSortType)[0] ? <span>&darr;</span> : <span>&uarr;</span>}</p>
+        </div>
+        <ul className={`absolute top-12 left-0 z-10 divide-y-2 bg-white border-gray-300 border-2 rounded-md w-full shadow-md ${!isClickSort ? "hidden" : "block"} shadow-black `}>
+          {sortTypes.map((list) => {
+            return <li key={list} className="px-3 py-3 justify-center w-full text-sm font-semibold text-gray-500 flex items-center cursor-pointer" onClick={() => sortingModel(list)}><p>{list} {sortTypeState(list)[0] ? <span>&darr;</span> : <span>&uarr;</span>}</p></li>
+          })}
+        </ul>
+      </div>
+
+    </div >
+
   );
 }
 
