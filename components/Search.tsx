ea92fdfa0178f436fd3e94c39be5dc31edb31b4a
Youngjae Lee <lyj@pspace.ai>
2022-09-07 16:47:51 +0900

    refactoring : getModels by sortType, isDesc, filterByName

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import { ModelInfos } from "pages/models";
-import React, { Dispatch, SetStateAction, useState } from "react";
-
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 interface props {
   setModels: Dispatch<SetStateAction<ModelInfos | undefined>>
   isClickSort: boolean,
@@ -14,37 +13,53 @@ type query = {
 }
 type sortType = "Last Added" | "Size" | "Alphabetic"
 type srotTypes = sortType[]
+interface isDescOfSortType {
+  lastAdded: boolean,
+  size: boolean,
+  alphabetic: boolean
+  [prop: string]: boolean;
+}
 
 function SearchBar({ setModels, isClickSort, closeSortingModel, setIsClickSort }: props) {
 
+  const defaultIsDesc = {
+    lastAdded: true,
+    size: true,
+    alphabetic: false
+  }
+
   const sortTypes: srotTypes = ["Last Added", "Size", "Alphabetic"]
   const [currentSortType, setCurrentSortType] = useState<string>(sortTypes[0])
-  const isDescOfLastAdded = useState<boolean>(true)
-  const isDescOfSize = useState<boolean>(true)
-  const isDescOfAlphabetic = useState<boolean>(false)
+  const [isDesc, setIsDesc] = useState<isDescOfSortType>({ ...defaultIsDesc })
   const [filterByName, setFilterByName] = useState<string>("")
   const [inputValue, setInputValue] = useState<string>("");
 
+  useEffect(() => {
 
+    (async () => {
+      console.log(filterByName)
+      await getModels(currentSortType, filterByName);
+    })();
+  }, [currentSortType, JSON.stringify(isDesc), filterByName])
 
   const sortingModel = async (type: string) => {
-    const [isDescState, setIsDescState] = sortTypeIsDesc(type)
-    let isDesc = isDescState
+    const sortType = getSortTypeKey(type)
+    const sortTypeIsDesc = isDesc[sortType]
     if (type === currentSortType) {
-      setIsDescState(prev => !prev)
-      isDesc = !isDesc
+      isDesc[sortType] = !sortTypeIsDesc
+      setIsDesc({ ...isDesc })
+    } else {
+      setCurrentSortType(type)
     }
-
-    setCurrentSortType(type)
-
-    await getModels(type, isDesc, filterByName);
-
     closeSortingModel();
   }
 
+  const getSortTypeKey = (type: string): string => {
+    const sortType = (type.charAt(0).toLowerCase() + type.slice(1).replace(" ", ""));
+    return sortType
+  }
 
   const searchModel = async (e?: React.KeyboardEvent<HTMLInputElement>) => {
-
     if (e?.key !== 'Enter') {
       return
     }
@@ -52,26 +67,7 @@ function SearchBar({ setModels, isClickSort, closeSortingModel, setIsClickSort }
     if (filterByName === inputValue) {
       return
     }
-
     setFilterByName(inputValue)
-
-    const isDescState = sortTypeIsDesc(currentSortType)
-
-    await getModels(currentSortType, isDescState[0], inputValue)
-
-  }
-
-  const sortTypeIsDesc = (type: string): [boolean, Dispatch<SetStateAction<boolean>>] => {
-    switch (type) {
-      case "Last Added":
-        return isDescOfLastAdded
-      case "Size":
-        return isDescOfSize
-      case "Alphabetic":
-        return isDescOfAlphabetic
-      default:
-        return isDescOfLastAdded
-    }
   }
 
   const getQuery = (type: string, isDesc: boolean, filterByName: string) => {
@@ -80,17 +76,16 @@ function SearchBar({ setModels, isClickSort, closeSortingModel, setIsClickSort }
     let query: query = {
       sort: sort,
     }
-
     if (name) {
       query = { ...query, filterByName: name }
     }
-
     const queryString = new URLSearchParams(query).toString();
     return queryString
   }
 
-  const getModels = async (type: string, isDescState: boolean, filterByName: string) => {
-    const query = getQuery(type, isDescState, filterByName);
+  const getModels = async (type: string, filterByName: string) => {
+    const isDescOfSort = isDesc[getSortTypeKey(type)]
+    const query = getQuery(type, isDescOfSort, filterByName);
     const { data, error } = await fetch(`/api/models?${query}`, {
       method: "GET",
     }).then((res) => res.json())
@@ -104,6 +99,21 @@ function SearchBar({ setModels, isClickSort, closeSortingModel, setIsClickSort }
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value)
+  }
+
+  const arrowIcon = (sortType: string): React.ReactElement<HTMLSpanElement> => {
+    const wahtArrow = (isDesc: boolean) => {
+      if (isDesc) {
+        return <span>&darr;</span>
+      }
+      return <span>&uarr;</span>
+    }
+
+    if (sortType === currentSortType) {
+      return wahtArrow(!isDesc[getSortTypeKey(sortType)])
+    }
+    return wahtArrow(isDesc[getSortTypeKey(sortType)])
+
   }
 
   return (
@@ -121,9 +131,11 @@ function SearchBar({ setModels, isClickSort, closeSortingModel, setIsClickSort }
         </div>
 
         <div className="pr-3 flex items-center cursor-pointer"
-          onClick={() => searchModel()}
+
         >
-          <Image src="/searchIcon.png" width="20px" height="20px" alt="find model" />
+          <Image src="/searchIcon.png" width="20px" height="20px" alt="find model"
+            onClick={() => searchModel()}
+          />
         </div>
       </div>
       <div id="sorting" className="rounded-md ring-2 ring-gray-300 md:w-1/6 w-[22%] text-center flex justify-center items-center relative cursor-pointer">
@@ -132,18 +144,18 @@ function SearchBar({ setModels, isClickSort, closeSortingModel, setIsClickSort }
             setIsClickSort(!isClickSort)
           }}
         >
-          <div className="w-full break-words md:text-sm text-[11px] px-1">{currentSortType} {sortTypeIsDesc(currentSortType)[0] ? <span>&darr;</span> : <span>&uarr;</span>}</div>
+          <div className="w-full break-words md:text-sm text-[11px] px-1">{currentSortType} {isDesc[getSortTypeKey(currentSortType)] ? <span>&darr;</span> : <span>&uarr;</span>}</div>
         </div>
         <ul className={`absolute top-12 left-0 z-10 divide-y-2 bg-white border-gray-300 border-2 rounded-md w-full shadow-md ${!isClickSort ? "hidden" : "block"} shadow-black `}>
           {sortTypes.map((list) => {
-            return <li key={list} className="md:px-3  px-2 md:py-3 py-2  justify-center w-full text-sm font-semibold text-gray-500 flex items-center cursor-pointer" onClick={() => sortingModel(list)}>
-              <p className="w-full break-words md:text-sm text-[11px] md:leading-normal leading-tight ">{list} {sortTypeIsDesc(list)[0] ? <span>&darr;</span> : <span>&uarr;</span>}
+            return <li key={list} className="md:px-3  px-2 md:py-3 py-2  justify-center w-full text-sm font-semibold text-gray-500 flex items-center cursor-pointer"
+              onClick={() => sortingModel(list)}>
+              <p className="w-full break-words md:text-sm text-[11px] md:leading-normal leading-tight ">{list} {arrowIcon(list)}
               </p>
             </li>
           })}
         </ul>
       </div>
-
     </div >
 
   );
