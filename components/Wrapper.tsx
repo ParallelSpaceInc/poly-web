ddee440018248570f0564aefad492ac78fc75fdb
Taehyeong Kim <kth@pspace.ai>
2022-09-07 14:46:44 +0900

    impl : upload button and adjust css

--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Wrapper({ children }: React.PropsWithChildren) {
   return (
     <div className="flex flex-col max-h-[10/11] overflow-auto">
-      <div className="flex-1 my-10 relative w-auto mx-6 max-w-7xl lg:px-8">
+      <div className="flex flex-col my-10 relative mx-auto max-w-7xl px-8">
         {children}
       </div>
     </div>
