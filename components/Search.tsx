87c68258f129c1b7bd5d98b2a450b50d9cecc7bf
Youngjae Lee <lyj@pspace.ai>
2022-09-07 17:04:30 +0900

    refactoring : getModels by sortType, isDesc, filterByName

--- components/Search.tsx
+++ components/Search.tsx
@@ -35,11 +35,11 @@ function SearchBar({ setModels, isClickSort, closeSortingModel, setIsClickSort }
   const [inputValue, setInputValue] = useState<string>("");
 
   useEffect(() => {
-
     (async () => {
-      console.log(filterByName)
       await getModels(currentSortType, filterByName);
+
     })();
+
   }, [currentSortType, JSON.stringify(isDesc), filterByName])
 
   const sortingModel = async (type: string) => {
@@ -60,6 +60,7 @@ function SearchBar({ setModels, isClickSort, closeSortingModel, setIsClickSort }
   }
 
   const searchModel = async (e?: React.KeyboardEvent<HTMLInputElement>) => {
+
     if (e?.key !== 'Enter') {
       return
     }
@@ -67,6 +68,7 @@ function SearchBar({ setModels, isClickSort, closeSortingModel, setIsClickSort }
     if (filterByName === inputValue) {
       return
     }
+
     setFilterByName(inputValue)
   }
 
@@ -84,6 +86,7 @@ function SearchBar({ setModels, isClickSort, closeSortingModel, setIsClickSort }
   }
 
   const getModels = async (type: string, filterByName: string) => {
+
     const isDescOfSort = isDesc[getSortTypeKey(type)]
     const query = getQuery(type, isDescOfSort, filterByName);
     const { data, error } = await fetch(`/api/models?${query}`, {
@@ -101,21 +104,6 @@ function SearchBar({ setModels, isClickSort, closeSortingModel, setIsClickSort }
     setInputValue(e.target.value)
   }
 
-  const arrowIcon = (sortType: string): React.ReactElement<HTMLSpanElement> => {
-    const wahtArrow = (isDesc: boolean) => {
-      if (isDesc) {
-        return <span>&darr;</span>
-      }
-      return <span>&uarr;</span>
-    }
-
-    if (sortType === currentSortType) {
-      return wahtArrow(!isDesc[getSortTypeKey(sortType)])
-    }
-    return wahtArrow(isDesc[getSortTypeKey(sortType)])
-
-  }
-
   return (
 
     <div className="w-full flex justify-between mt-6">
@@ -131,11 +119,9 @@ function SearchBar({ setModels, isClickSort, closeSortingModel, setIsClickSort }
         </div>
 
         <div className="pr-3 flex items-center cursor-pointer"
-
+          onClick={() => searchModel()}
         >
-          <Image src="/searchIcon.png" width="20px" height="20px" alt="find model"
-            onClick={() => searchModel()}
-          />
+          <Image src="/searchIcon.png" width="20px" height="20px" alt="find model" />
         </div>
       </div>
       <div id="sorting" className="rounded-md ring-2 ring-gray-300 md:w-1/6 w-[22%] text-center flex justify-center items-center relative cursor-pointer">
@@ -150,12 +136,13 @@ function SearchBar({ setModels, isClickSort, closeSortingModel, setIsClickSort }
           {sortTypes.map((list) => {
             return <li key={list} className="md:px-3  px-2 md:py-3 py-2  justify-center w-full text-sm font-semibold text-gray-500 flex items-center cursor-pointer"
               onClick={() => sortingModel(list)}>
-              <p className="w-full break-words md:text-sm text-[11px] md:leading-normal leading-tight ">{list} {arrowIcon(list)}
+              <p className="w-full break-words md:text-sm text-[11px] md:leading-normal leading-tight ">{list} {isDesc[getSortTypeKey(currentSortType)] ? <span>&darr;</span> : <span>&uarr;</span>}
               </p>
             </li>
           })}
         </ul>
       </div>
+
     </div >
 
   );
