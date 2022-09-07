9d2ec73efd1c71e06a4a6f9d97a0ebed7dbf5977
Taehyeong Kim <kth@pspace.ai>
2022-09-07 15:14:39 +0900

    css, fix : restore w-full option

--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Wrapper({ children }: React.PropsWithChildren) {
   return (
     <div className="flex flex-col max-h-[10/11] overflow-auto">
-      <div className="flex flex-col my-10 relative mx-auto max-w-7xl px-8">
+      <div className="flex flex-col my-10 relative w-full mx-auto max-w-7xl px-8">
         {children}
       </div>
     </div>
