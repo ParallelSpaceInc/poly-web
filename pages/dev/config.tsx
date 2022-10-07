884b0153eeaeac917a57f9456ae96c83e110dded
Taehyeong Kim <kth@pspace.ai>
2022-10-07 12:39:48 +0900

    page : add Configuration page

---
+++ pages/dev/config.tsx
@@ -0,0 +1,8 @@
+import Wrapper from "@components/Wrapper";
+import type { NextPage } from "next";
+
+const PageConfing: NextPage = () => {
+  return <Wrapper>hello</Wrapper>;
+};
+
+export default PageConfing;
