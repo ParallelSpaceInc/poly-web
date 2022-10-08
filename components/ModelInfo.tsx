4b8a4c296fa8a97910761664cf6b09350f2a5bcc
Min Lee <mklee@pspace.ai>
2022-10-08 15:33:57 +0900

    Modify model viewer's UI

--- components/ModelInfo.tsx
+++ components/ModelInfo.tsx
@@ -6,20 +6,20 @@ const ModelInfo = ({ modelId }: { modelId: string }) => {
   return (
     <div className="flex justify-start p-3 space-x-5">
       <div className="flex flex-col">
-        <div className="m-auto text-lg">Vertex Count</div>
-        <div className="m-auto ">
+        <div className="m-auto text-gray-500">Vertices</div>
+        <div className="m-auto text-gray-500 font-bold">
           {AddUnit(model.data?.modelVertex) ?? "unknown"}
         </div>
       </div>
       <div className="flex flex-col">
-        <div className="m-auto text-lg">Triangle Count</div>
-        <div className="m-auto">
+        <div className="m-auto text-gray-500">Triangles</div>
+        <div className="m-auto text-gray-500 font-bold">
           {AddUnit(model.data?.modelTriangle) ?? "unknown"}
         </div>
       </div>
       <div className="flex flex-col">
-        <div className="m-auto text-lg">Model Size</div>
-        <div className="m-auto">
+        <div className="m-auto text-gray-500">Model Size</div>
+        <div className="m-auto text-gray-500 font-bold">
           {AddUnit(model.data?.modelSize) + "B" ?? "unknown"}
         </div>
       </div>
