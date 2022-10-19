ebd5ce64638cc71992bf58c3b7de51a3b88621dd
Taehyeong Kim <kth@pspace.ai>
2022-10-19 09:21:25 +0900

    impl : model select mode

--- components/Thumbnails.tsx
+++ components/Thumbnails.tsx
@@ -5,6 +5,8 @@ import { useRouter } from "next/router";
 import { increaseView } from "pages/models/[id]";
 import { MouseEvent, useState } from "react";
 
+type pageMode = "default" | "select";
+
 function Thumbnails({
   loading,
   modelInfos,
@@ -15,21 +17,45 @@ function Thumbnails({
   devMode?: boolean;
 }) {
   const router = useRouter();
+  const [selectedModels, setSelectedModels] = useState<string[]>([]);
+  const [mode, setMode] = useState<pageMode>("default");
   return (
     <div className="mt-10 grid grid-cols-2 gap-x-4 gap-y-4 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
       {!loading && modelInfos ? (
         modelInfos.map((info, i) => (
           <div key={i} className="flex flex-col relative cursor-pointer">
+            <div>{mode}</div>
             <div
-              className="block aspect-[4/3] relative rounded transitiongit  hover:shadow-md"
-              onClick={() => router.push(`/models/${info.id}`)}
+              className={`block aspect-[4/3] relative rounded hover:shadow-md ${
+                mode === "select" && selectedModels.includes(info.id)
+                  ? "border border-blue-300"
+                  : ""
+              }`}
+              onClick={() => {
+                if (mode === "select") {
+                  if (selectedModels.includes(info.id)) {
+                    setSelectedModels((prev) =>
+                      prev.filter((val) => val !== info.id)
+                    );
+                  } else {
+                    setSelectedModels((prev) => [...prev, info.id]);
+                  }
+                } else {
+                  router.push(`/models/${info.id}`);
+                }
+              }}
             >
               <Image
                 src={info.thumbnailSrc ? info.thumbnailSrc : "/cube.png"}
                 alt={info.name}
                 layout="fill"
                 objectFit="cover"
-                className="rounded"
+                draggable="false"
+                className={`rounded ${
+                  mode === "select" && !selectedModels.includes(info.id)
+                    ? "opacity-30"
+                    : "opacity-100"
+                }`}
                 loading="lazy"
               />
             </div>
@@ -75,6 +101,35 @@ function Thumbnails({
       ) : (
         <span>Loading...</span>
       )}
+      {devMode ? (
+        <>
+          <div className="fixed flex flex-col right-5 top-32 rounded bg-slate-200 w-32 text-center select-none cursor-pointer [&>*]:p-2">
+            <div
+              onClick={(e) => {
+                mode === "default" ? setMode("select") : setMode("default");
+              }}
+            >
+              {mode === "default" ? "선택모드 켜기" : "선택모드 끄기"}
+            </div>
+          </div>
+
+          <div
+            id="selectMenu"
+            className={`fixed flex flex-col right-20 bottom-52 rounded bg-slate-200 w-32 text-center select-none cursor-pointer [&>*]:p-2 ${
+              mode !== "select" ? "hidden" : ""
+            } `}
+          >
+            <div
+              className="hidden rounded bg-blue-300"
+              onClick={() => {
+                handleHideRequest();
+              }}
+            >
+              숨김처리하기
+            </div>
+          </div>
+        </>
+      ) : null}
     </div>
   );
 }
@@ -116,6 +171,8 @@ const IconWithCounter = ({
     </div>
   );
 };
+
+const handleHideRequest = () => {};
 
 interface ImageAttributes {
   src: string;
