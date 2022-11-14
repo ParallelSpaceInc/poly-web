4094c71a7200f7cb929a3a9217cf39856f0e676f
Taehyeong Kim <kth@pspace.ai>
2022-11-14 15:26:19 +0900

    impl : progress bar on model viewer

--- components/Model.tsx
+++ components/Model.tsx
@@ -1,7 +1,16 @@
 import { ModelInfo } from "@customTypes/model";
 import "@google/model-viewer";
+import { round } from "lodash";
+import { useEffect, useState } from "react";
 
 const Model = ({ info }: { info: ModelInfo }) => {
+  const [loading, setLoading] = useState<number>(0);
+  useEffect(() => {
+    const modelComponent = document.getElementById("modelViewer");
+    modelComponent?.addEventListener("progress", (xhr: any) => {
+      setLoading(xhr.detail.totalProgress);
+    });
+  }, []);
   const parsed = {
     src: `${info.modelSrc}`,
     "ios-src": info.usdzSrc ?? "",
@@ -21,7 +30,42 @@ const Model = ({ info }: { info: ModelInfo }) => {
     // "skybox-image":
     //   "https://modelviewer.dev/assets/whipple_creek_regional_park_04_1k.hdr",
   };
-  return <model-viewer id="modelViewer" {...parsed} />;
+
+  return (
+    <div className="flex w-full h-full justify-center items-center ">
+      <div
+        className={`w-full h-full ${
+          loading === 1 ? "opacity-100" : "opacity-80"
+        }`}
+      >
+        <model-viewer id="modelViewer" {...parsed} />
+      </div>
+      <LoadingBar progress={loading} />
+    </div>
+  );
+};
+
+const LoadingBar = ({ progress }: any) => {
+  return (
+    <div
+      className={`flex flex-col w-full max-w-xs transition-opacity duration-500 fixed ${
+        progress === 1 ? "opacity-0" : "opacity-100"
+      }`}
+    >
+      <div className="text-center m-2">Loading Model</div>
+      <div
+        className={`flex bg-gray-300 rounded-full dark:bg-gray-700 
+        }`}
+      >
+        <div
+          className="bg-slate-400 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
+          style={{ width: `${progress * 100}%` }}
+        >
+          {round(progress * 100, 1)}%
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Model;
