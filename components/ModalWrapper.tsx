6fd9e82fc4be59a6e7c1fe2769b472ae7da245f1
Brian <ballow348@gmail.com>
2022-11-22 13:09:53 +0900

    impl : make scroll bar for modal

--- components/ModalWrapper.tsx
+++ components/ModalWrapper.tsx
@@ -15,7 +15,7 @@ function ModalWrapper({
       className="backlayer sm:fixed sm:top-0 sm:left-0 sm:w-screen sm:h-screen sm:bg-black sm:bg-opacity-50"
       onClick={handleClick}
     >
-      <div className="flex flex-col sm:bg-white p-4 sm:m-10 sm:rounded">
+      <div className="flex flex-col p-4 sm:h-[90%] sm:scroll sm:overflow-y-scroll scroll-m-4 sm:bg-white sm:m-10 sm:rounded">
         {children}
       </div>
     </div>
